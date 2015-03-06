b3337767332fcc142c3e19f1d59940a99b21f288
JohnPogi <johncaingles@gmail.com>
2015-03-07 02:47:01 +0800

    NO MORE HEADER YO BACHINI POWERS

--- app/src/main/java/com/example/john/thegreatbigidea/MainActivity.java
+++ app/src/main/java/com/example/john/thegreatbigidea/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceSta te);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
