1a29c0832fd212397c3cbf355724530f9b1ad1d2
JohnPogi <johncaingles@gmail.com>
2015-03-07 10:13:55 +0800

    finished HACKATHON OH YEAAAAAAAAAAH

--- app/src/main/java/com/example/john/thegreatbigidea/ScreenSlidePageFragment.java
+++ app/src/main/java/com/example/john/thegreatbigidea/ScreenSlidePageFragment.java
@@ -1,15 +1,22 @@
 package com.example.john.thegreatbigidea;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 
@@ -30,10 +37,46 @@ public class ScreenSlidePageFragment extends Fragment
         Drawable drawable = res.getDrawable(resID );
         img.setImageDrawable(drawable);
 
-        /*Matrix matrix = new Matrix();
-        img.setScaleType(ImageView.ScaleType.MATRIX);   //required
-        matrix.postRotate((float) 90, pivX, pivY);
-        img.setImageMatrix(matrix);*/
+        ImageButton save = (ImageButton) view.findViewById(R.id.imageButton);
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), MainMenu.class);
+                startActivity(i);
+            }});
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*String mtext = "";
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Image saved!");
+
+// Set up the input
+                final EditText input;
+                input = new EditText(this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+// Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mtext = input.getText().toString();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();*/
+            }
+        });
+
         return view;
     }
 }
