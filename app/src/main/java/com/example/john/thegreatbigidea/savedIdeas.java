b46e8035f3d79d45638f2651e3a1f445b2efa35e
JohnPogi <johncaingles@gmail.com>
2015-03-28 03:36:58 +0800

    added back to define screen

--- app/src/main/java/com/example/john/thegreatbigidea/savedIdeas.java
+++ app/src/main/java/com/example/john/thegreatbigidea/savedIdeas.java
@@ -1,12 +1,12 @@
 package com.example.john.thegreatbigidea;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class savedIdeas extends ActionBarActivity {
+public class savedIdeas extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
