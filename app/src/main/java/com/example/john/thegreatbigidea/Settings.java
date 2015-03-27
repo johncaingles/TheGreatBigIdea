b446c38933632ddf4b4743f74e4548fe17f1100e
Manto <Retiacca@user.noreply.github.com>
2015-03-28 03:38:33 +0800

    sex master commit to fuck

--- app/src/main/java/com/example/john/thegreatbigidea/Settings.java
+++ app/src/main/java/com/example/john/thegreatbigidea/Settings.java
@@ -1,6 +1,6 @@
 package com.example.john.thegreatbigidea;
 
-import android.support.v7.app.ActionBarActivity;
+import  android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
--- app/src/main/java/com/example/john/thegreatbigidea/savedIdeas.java
+++ app/src/main/java/com/example/john/thegreatbigidea/savedIdeas.java
@@ -1,17 +1,38 @@
 package com.example.john.thegreatbigidea;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class savedIdeas extends ActionBarActivity {
+
+    private Button backBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_ideas);
+        backBtn = (Button) findViewById(R.id.button);
+
+        backBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(savedIdeas.this,MainMenu.class );
+                startActivity(i);
+                finish();
+            }
+
+
+            });
+
+
     }
 
 
